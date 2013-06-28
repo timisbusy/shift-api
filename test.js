9d638e5c673c7aa698e4698b4ecd4b5ebc82afde
Tim Atkinson <timisbusy@gmail.com>
2013-06-28 15:09:18 -0400

    getting set up

---
+++ test.js
@@ -0,0 +1,18 @@
+var Shift = require('./index');
+
+var creds = require('./conf');
+
+var shift = new Shift(creds.shift);
+
+
+var newMessage = {
+  text: "Heya, fellers. Happy Friday!",
+  "addressed_teams": [
+    "7953133a-0cf2-4416-b7ee-8442f53904ea"
+  ]
+};
+
+shift.messages.send(newMessage, function (err, res) {
+  if (err) { throw err; }
+  console.log(res);
+});
