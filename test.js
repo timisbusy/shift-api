7d3321192129c0bf54e1165ea4e2d5e1928c2dad
Tim Atkinson <timisbusy@gmail.com>
2013-07-08 13:56:10 -0400

    update tests and fix user auth

--- test.js
+++ test.js
@@ -5,14 +5,19 @@ var creds = require('./conf');
 var shift = new Shift(creds.shift);
 
 
-var newMessage = {
-  text: "Heya, fellers. Happy Friday!",
-  "addressed_teams": [
-    "7953133a-0cf2-4416-b7ee-8442f53904ea"
-  ]
-};
+// var newMessage = {
+//   text: "Heya, fellers. Happy Friday!",
+//   "addressed_teams": [
+//     "7953133a-0cf2-4416-b7ee-8442f53904ea"
+//   ]
+// };
 
-shift.messages.send(newMessage, function (err, res) {
+// shift.messages.send(newMessage, function (err, res) {
+//   if (err) { throw err; }
+//   console.log(res);
+// });
+
+shift.users.me(creds.testUserToken, function (err, res) {
   if (err) { throw err; }
   console.log(res);
 });
